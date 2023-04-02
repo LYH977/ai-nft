b78af8ee5f17a8c63f260ad947fb42c4e70eafef
Lee Yuan Hooi <ho0i97@hotmail.com>
2023-04-02 23:47:38 +0800

    moved tests folder out of pages

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react'
 
 import '@testing-library/jest-dom'
-import Home from '..'
+import Home from '../pages'
 
 describe('Homepange Ui unit test', () => {
 
